1c49deadd90cda8a6eb8ed8a702c730b1ee134d6
Dominik Tränklein <dominik@bleech.de>
2018-08-12 15:23:42 +0200

    feat(Recipes\Wpml): add gravityforms multilingual

---
+++ src/Recipes/Wpml/GravityformsMultilingual.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FlyntWP\PremiumPluginDownloader\Recipes\Wpml;
+
+class GravityformsMultilingual extends BaseRecipe
+{
+    protected $downloadId = '8882';
+
+    protected $latestVersion = '1.3.17';
+}
