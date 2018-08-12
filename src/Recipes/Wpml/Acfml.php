7a5e27163a2ccb49877afb429d0062b94cdc264e
Dominik Tränklein <dominik@bleech.de>
2018-08-12 15:23:14 +0200

    feat(Recipes\Wpml): add acfml

---
+++ src/Recipes/Wpml/Acfml.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FlyntWP\PremiumPluginDownloader\Recipes\Wpml;
+
+class Acfml extends BaseRecipe
+{
+    protected $downloadId = '1097589';
+
+    protected $latestVersion = '0.7';
+}
