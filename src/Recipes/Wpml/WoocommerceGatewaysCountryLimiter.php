febd469fadfbafa8ee056c87129951d2bb0a874d
Dominik Tränklein <dominik@bleech.de>
2018-08-12 15:24:11 +0200

    feat(Recipes\Wpml): add woocommerce gateways country limiter

---
+++ src/Recipes/Wpml/WoocommerceGatewaysCountryLimiter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FlyntWP\PremiumPluginDownloader\Recipes\Wpml;
+
+class WoocommerceGatewaysCountryLimiter extends BaseRecipe
+{
+    protected $downloadId = '361267';
+
+    protected $latestVersion = '1.4';
+}
