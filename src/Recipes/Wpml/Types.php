e30ca020d09fc1eaae493f5651930f0ef52f1249
Dominik Tränklein <dominik@bleech.de>
2018-08-12 15:23:54 +0200

    feat(Recipes\Wpml): add (toolset) types

---
+++ src/Recipes/Wpml/Types.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FlyntWP\PremiumPluginDownloader\Recipes\Wpml;
+
+class Types extends BaseRecipe
+{
+    protected $downloadId = '1385906';
+
+    protected $latestVersion = '3.0.7';
+}
