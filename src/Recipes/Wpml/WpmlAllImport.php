122f7847e568a44dd36f2788e96305f921c3a1ee
Dominik Tränklein <dominik@bleech.de>
2018-08-12 15:24:33 +0200

    feat(Recipes\Wpml): add wpml all import

---
+++ src/Recipes/Wpml/WpmlAllImport.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FlyntWP\PremiumPluginDownloader\Recipes\Wpml;
+
+class WpmlAllImport extends BaseRecipe
+{
+    protected $downloadId = '720221';
+
+    protected $latestVersion = '2.0.6';
+}
