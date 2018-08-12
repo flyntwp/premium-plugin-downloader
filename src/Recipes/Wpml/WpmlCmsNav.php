b2e5a9a55858648f3ed05ede26f8dedf325844de
Dominik Tränklein <dominik@bleech.de>
2018-08-12 15:25:02 +0200

    feat(Recipes\Wpml): add wpml cms nav

---
+++ src/Recipes/Wpml/WpmlCmsNav.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FlyntWP\PremiumPluginDownloader\Recipes\Wpml;
+
+class WpmlCmsNav extends BaseRecipe
+{
+    protected $downloadId = '6096';
+
+    protected $latestVersion = '1.4.23';
+}
