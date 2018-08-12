cb3134b4723aab5bd9b4aee7fed729284aa65779
Dominik Tränklein <dominik@bleech.de>
2018-08-12 15:25:27 +0200

    feat(Recipes\Wpml): add wpml string translation

---
+++ src/Recipes/Wpml/WpmlStringTranslation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FlyntWP\PremiumPluginDownloader\Recipes\Wpml;
+
+class WpmlStringTranslation extends BaseRecipe
+{
+    protected $downloadId = '6092';
+
+    protected $latestVersion = '2.8.5';
+}
