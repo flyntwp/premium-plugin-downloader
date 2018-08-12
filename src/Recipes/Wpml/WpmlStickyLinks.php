ee8b8ba1dcff6fc7cadbe647a275947d46840b2d
Dominik Tränklein <dominik@bleech.de>
2018-08-12 15:25:19 +0200

    feat(Recipes\Wpml): add wpml sticky links

---
+++ src/Recipes/Wpml/WpmlStickyLinks.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FlyntWP\PremiumPluginDownloader\Recipes\Wpml;
+
+class WpmlStickyLinks extends BaseRecipe
+{
+    protected $downloadId = '6090';
+
+    protected $latestVersion = '1.4.4';
+}
